613652f86a85b8b2358fb245b9a09428f0b6efaf
Ankit Gupta <ankit.gupta2801@gmail.com>
2016-01-19 23:01:17 -0800

    initial commit

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,8 @@ Rails.application.routes.draw do
   # You can have the root of your site routed with "root"
   root 'home#index'
 
+  get 'fitbit/:resource/:date.json' => 'fitbit_api#data_request'
+
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
